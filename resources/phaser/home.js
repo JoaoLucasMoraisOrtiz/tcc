c5386f78a968c00d0507dc804b76208e9c1cd423
JoaoLucasMoraisOrtiz <jlucasmortiz@hotmail.com>
2022-04-11 13:28:30 -0300

    Criado a rederizaçao das cartas de açao, e comecei a colocar as plantas como dropZones. Vendo se e melhor pensar novamente na estrutura das plantas, criando uma outra forma de as exibir em apenas uma imagem, e nao em trexos de codigo HTML por ser mais facil para lidar no phaser. Proximo passo sera determinar em qual planta a carta foi dropada.

--- resources/phaser/home.js
+++ resources/phaser/home.js
@@ -2,6 +2,10 @@
 plantsHeight = document.getElementById('game').clientHeight
 plantsWidth = document.getElementById('game').clientWidth
 
+var load = { "card": 2, 
+             "locationsCards": ['../../../resources/view/pages/img/bgImage.jpg', '../../../resources/view/pages/img/bgImage.jpg', '../../../resources/view/pages/img/bgImage.jpg'],
+             "images": [['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png'], ['../../../resources/view/pages/img/botao.png', '../../../resources/view/pages/img/Tronco.png', '../../../resources/view/pages/img/Fe.png', '../../../resources/view/pages/img/Fd.png']]}
+
 //variável de configuração do phaser com openGL
 var config = {
     type: Phaser.WEBGL,
@@ -23,31 +27,64 @@ var game = new Phaser.Game(config)
 
 
 //pré carrega os assets do jogo
-function preload ()
-{
-    this.load.image('card', '../../../resources/view/pages/img/bgImage.jpg');
+function preload() {
+    for (let index = 0; index <= load.card; index++) {
+
+        this.load.image('card' + index, load.locationsCards[index]);
+
+    }
+
+    for (let index = 0; index <= 8; index++) {
+        
+        this.load.image('button'+index, load.images[index][0]);
+    }
 }
 
 //cria o sprite do jogo
-function create ()
-{
+function create() {
 
-    //cria o objeto dragable da carta
-    var image = this.add.sprite(200, plantsHeight, 'card').setInteractive();
-    
-    //remodela  a imágem pra ela ficar na forma da carta
-    image.setScale(0.2, 0.5)
-    this.input.setDraggable(image);
+
+    //cria as cartas de ação
+    for (let index = 0; index <= load.card; index++) {
+
+        if (load.card < 7) {
+            //cria o objeto dragable da carta
+            var cards = this.add.sprite(200 + (200 * index * 0.7), plantsHeight, 'card' + index).setInteractive();
+
+        } else {
+            //cria o objeto dragable da carta
+            var cards = this.add.sprite(200 + (200 * index * 0.3), plantsHeight, 'card' + index).setInteractive();
+
+        }
+        //remodela  a imagem pra ela ficar na forma da carta
+        cards.setScale(0.2, 0.5)
+        this.input.setDraggable(cards);
+    }
+
+    //cria as plantas
+    li = 0;
+    x = 250;
+    y = 150;
+    for (let index = 0; index <= 8; index++) {
+
+        if(li > 2){
+            x = 250;
+            y = y + 100;
+            li = 0;
+        }
+        var button = this.add.image(x, y, 'button'+index).setInteractive();
+        button.input.dropZone = true;
+        button.setName('plant'+index);
+        x = x + 100;
+        li = li + 1;
+    }
 
     //  The pointer has to move 16 pixels before it's considered as a drag
-    this.input.dragDistanceThreshold = 16;
-
-    //cria uma luz
-    var spotlight = this.lights.addLight(-1, 300, 280).setIntensity(3);
+    this.input.dragDistanceThreshold = 1;
 
     //função que efetivamente arrasta o objeto, pegando sua posição X e Y
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        
+
         //modendo o objeto para X e Y
         gameObject.x = dragX;
         gameObject.y = dragY;
@@ -61,15 +98,15 @@ function create ()
     //determina que quando o objeto parar de ser arrastado, ele irá voltar a cor padrão
     this.input.on('dragend', function (pointer, gameObject, dropped) {
 
-        if(!dropped){
+        if (!dropped) {
             gameObject.x = gameObject.input.dragStartX;
             gameObject.y = gameObject.input.dragStartY;
         }
-        graphics.clear();
-        graphics.lineStyle(2, 0xffff00);
-        graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+        
         gameObject.clearTint()
-        if(dropped){
+
+        if (dropped) {
+            console.log();
             gameObject.setActive(false).setVisible(false);
             window.alert('wow')
             gameObject.x = gameObject.input.dragStartX;
@@ -79,11 +116,6 @@ function create ()
 
     });
 
-    //cria uma zona para dropar o objeto
-    var zone = this.add.zone(500, 300, 300, 300).setRectangleDropZone(300, 300);
-
     //  Just a visual display of the drop zone
-    var graphics = this.add.graphics();
-    graphics.lineStyle(2, 0xffff00);
-    graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+
 }
